9cf1448927b8bd425a288b6a910d017233978852
Nitax <lecongluyen9a1@gmail.com>
2026-05-31 17:31:38 +0700

    Complete Day 1 Python introduction exercises

---
+++ 01_Day_Introduction/helloworld.py
@@ -0,0 +1,23 @@
+# Introduction
+# Day 1 - 3DayOF0DaysOfPython Challenge
+print("Hello World!") # Print Hello World!
+
+
+print(2 + 3)                # addition(+)
+print(3 - 1)                # subtraction(-)
+print(2 * 3)                # multiplication(*)
+print(3 + 2)                # addition(+)
+print(3 - 2)                # subtraction(-)
+print(3 * 2)                # multiplication(*)
+print(3 / 2)                # division(/)
+print(3 ** 2)               # exponential(**)
+print(3 % 2)                # modulus(%)
+print(3 // 2)               # Floor division operator(//)
+
+#Checking data tpye
+print(type(10))             #Int
+print(type(10.50))          #Float
+print(type('Hello'))        #String
+print(type(["Apple","Banana","Cherrry"]))    #List
+print(type({'name' : 'Luyen'}))              #Dictionary : KEY-Value
+print(type({9.8 , 3.14 , 2}))                #Set -Tuple
